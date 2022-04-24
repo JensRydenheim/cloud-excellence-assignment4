49afe169e362cee88f2172b4338a823008402091
Jens Rydenheim <jens.rydenheim@gmail.com>
2022-04-24 13:46:50 +0200

    Read content from CosmosDB function

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Message from API: {{ message }}</div>
+  <div>
+    <h1>Softhouse</h1>
+    <div v-for="paragraph in paragraphs" :key="paragraph.id">
+      <h2>{{ paragraph.header }}</h2>
+      <p>{{ paragraph.content }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,12 +13,16 @@ export default {
   name: "App",
   data() {
     return {
-      message: "",
+      paragraphs: "",
     };
   },
   async mounted() {
-    const { body } = await (await fetch("/api/content?name=Softhouse")).json();
-    this.message = body;
+    const body = await (
+      await fetch(
+        "https://cosmosdbfunctionapp20220423133227.azurewebsites.net/api/page/1"
+      )
+    ).json();
+    this.paragraphs = body;
   },
 };
 </script>
